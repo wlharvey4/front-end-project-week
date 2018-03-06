bcd673fd3e2d844714bab6cc6d304b1124d4c91a
wlharvey4 <pinecone062@gmail.com>
2018-03-06 10:50:36 -0800

    Added Lorem content to create 9 notes;
    NoteData.js: added lorem-title package and lorem-paragraph package;
                 created function to fill 9 notes with dummy data;
    package.json, yarn.lock: updated with new packages.

--- lambda-notes/src/components/NoteData.js
+++ lambda-notes/src/components/NoteData.js
@@ -1,16 +1,34 @@
 /**
    NoteData.js
    ============
+   DATA FOR THE APP
+   --------------------------------------------------
    Version 0.1 2018-03-03T14:31:06
-   -------------------------------
-   Create dummy data
+   ..................................................
+   - create dummy data
+   __________________________________________________
+   Version 0.2 2018-03-06T10:17:16
+   ..................................................
+   - use some lorem ipsum packages to flesh out nine
+     cards
+   __________________________________________________
  */
 
-const NoteData = 
-  [
-    {id: 1, title: 'Title 1', contents: 'Contents 1'},
-    {id: 2, title: 'Title 2', contents: 'Contents 2'},
-    {id: 3, title: 'Title 3', contents: 'Contents 3'},
-  ]
+import randomTitle from 'random-title';
+import randomParagraph from 'random-paragraph';
 
+const NUM_NOTES = 9;
+
+const fillNotes = (num, data) => {
+  if (num === 0) return data;
+  return fillNotes(num - 1, (data.push(
+    {
+      id:       num,
+      title:    randomTitle(),
+      contents: randomParagraph()
+    })) && data
+  );
+}
+
+const NoteData = fillNotes(NUM_NOTES, []);
 export default NoteData;
